b3cd411ac2725f2be8b6b13e24f10535e5084d11
RoJooHee <rjh123012@naver.com>
2020-05-14 00:38:23 +0900

    문제풀이 2번째 과제

--- Assignment2/src/BinarySearchTree.java
+++ Assignment2/src/BinarySearchTree.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+import com.sun.source.tree.Tree;
 // 이진트리의 노드를 설정
 class Node<E extends Comparable<E>> {
 	private E item;
@@ -91,65 +93,54 @@ class BinaryTree<E extends Comparable<E>> {
 		return Size(r.getLeft())+Size(r.getRight())+1;
 	}
 	
-	/*  item1보다 크거나 같고, item2보다 작거나 같은 항목을 순서대로 출력한다.
-	public E Compare(E item1, E item2) { return Compare(root, item1, item2); }
-	public Node<E> Compare(Node<E> r, E item1, E item2) {
-		if (r == null) {return null;}  // 찾는 데이터가 없는 경우
-		int t = item1.compareTo(r.get());
-		if (t == 0)  (r.getRight());
-		else if (t < 0)  return r.getLeft();
-		else return r.getRight();}
-		//트리에 있는 노드 중에서 item1보다 같거나 크교, item2보다 같거나 작도록.
-		//비교하면서 for문?으로 해당범위만 출력되게??
-		//중위순회??
-	*/
+	public void Com(E item1, E item2) {Com(root, item1, item2);}
+	public void Com(Node<E> r, E item1, E item2) {
+		if (r==null) return;
+		int t = item1.compareTo(r.get()); //root와 a를 비교
+		int v = item2.compareTo(r.get()); //root와 b를 비교
+		if (t<0) Com(r.getLeft(), item1, item2); //만약 a<root이면 왼쪽으로 감
+		if (t>=0) { //그러다가 a>=root가 되면
+			System.out.println(r.get()); //그 r에 해당하는 노드를 출력하고
+			if (v>=0) Com1(r.getRight(), item1, item2); } //b<=root이면 r을 오른쪽으로 이동하고 Com1함수 출력
+		}
+		//t>=0이어서 오른쪽으로 이동하게되면 다시 t<0이 되는데, 그러면 또다시 왼쪽노드로 가야해서, Com1로 오른쪽으로 가는 함수 만듦
 	
-	/* 중위순회하면서 찾기??
-		public void Com(E item1, E item2) {Com(root, item1, item2);}
-		public void Com(Node<E> r, E item1, E item2) {
-			if (r == null) return;
-			if (r.get().compareTo(item1)>=0) {
-				InTrav(r.getLeft());
-				System.out.println(r.get());
-				InTrav(r.getRight()); }
-	}
-	*/
+	public void Com1(Node<E> r, E item1, E item2) {
+		if (r==null) return;
+		int v = item2.compareTo(r.get()); //root와 b를 비교
+		if (v>=0) { //만약 b>=root이면
+			System.out.println(r.get()); //그 r에 해당하는 노드를 출력하고
+			Com1(r.getRight(), item1, item2); } //r을 오른쪽으로 이동하고 다시 호출
+		}
 }
-
 public class BinarySearchTree {
 	public static void main(String[] args) throws Exception {
 		BinaryTree<String> tree = new BinaryTree<String>();
 		Scanner s=new Scanner(System.in);
 		while (true) {
-			System.out.print("Enter command : ");
+			System.out.print("명령 값 : ");
 			String cmd = s.next();
-			if (cmd.equals("q")) break;
+
+			if (cmd.equals("q")) break; //프로그램 종료
 			
-			if (cmd.equals("i")) {
-				System.out.print("넣을 항목 입력 : ");
+			if (cmd.equals("i")) { //트리에 추가
 				String x = s.next();
 				tree.Insert(x); }
 			
-			else if (cmd.equals("d")) {
-				System.out.print("삭제할 항목 입력 : ");
+			else if (cmd.equals("d")) { //항목 존재하는 경우 트리에서 삭제
 				String x = s.next();
 				tree.Remove(x);}
 			
-			else if (cmd.equals("f")) {
-				System.out.print("찾을 항목 입력 : ");
+			else if (cmd.equals("f")) { //있는지 검사하고 있으면 true, 없으면 false
 				String x = s.next();
 				String res = tree.Find(x);
 				if (res != null) System.out.println("true");
 				else System.out.println("false"); }
 			
-			else if (cmd.equals("s")) { //입력 s a b : a 보다 크거나 같고, b 보다 작거나 같은 항목을 순서대로 출력한다.
-				String a= s.next();
-				String b= s.next();
-				/*for(int i=0; i<tree.Size(); i++) {
-					tree.Com(a, b);
-						} */
-				}
-			tree.InTrav();
+			else if (cmd.equals("s")) { //a<=항목<=b 순서대로 출력
+				String a=s.next();
+				String b=s.next();
+				tree.Com(a, b);}
 		}
 	}
 }
